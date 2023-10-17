782aae6a27a324117f25c40535e955cac840c54d
NIDHISH <nidhish1233@gmail.com>
2023-10-17 13:21:35 +0530

    #

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = [".vercel.app", "http://127.0.0.1:8000/", "*"]
 
 # CSRF_TRUSTED_ORIGINS = []
 
